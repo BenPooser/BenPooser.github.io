4d7ee49f5a14dec8fee2021ec9b68a453c2531ee
BenPooser <pooserben@gmail.com>
2021-11-01 22:46:12 -0400

    Updates

---
+++ static/js/main.34ec3b11.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbenpooser.github.io"]=this["webpackJsonpbenpooser.github.io"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var o=n(1),r=n.n(o),s=n(3),c=n.n(s),i=n(0);var a=function(){return Object(i.jsx)("div",{className:"app",children:"hello"})};c.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.34ec3b11.chunk.js.map

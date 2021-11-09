3f8ee691e1d96812ef864e81d650427dca8ac8b4
BenPooser <pooserben@gmail.com>
2021-11-08 22:28:37 -0500

    Add timeline

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,71 +1,55 @@
 import "./experience.scss";
 import "../../global.scss"
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { ReactComponent as WorkIcon } from "./work.svg";
+import { ReactComponent as SchoolIcon } from "./school.svg";
+import timelineElements from "../../timelineElements.js";
+import 'react-vertical-timeline-component/style.min.css';
 
 export default function Experience() {
-    const items = [
-        {   
-            title: "May 1940",
-            cardTitle: "Software Engineer - Manhattan Associates",
-            // cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-            cardDetailedText: [
-                "Joined the Movers team heading the Putaway component, collaborating with team members overseas", 
-                "Practice Object-Oriented Programming techniques using Java and Spring Boot, creating JUnit tests", 
-                "Helped implement the compatibility of multiple items at once in the Putaway component using Agile", 
-                "Awarded Research & Development Team of the Quarter in April 2021"],
-            media: {
-              type: "IMAGE",
-              source: {
-                url: "./assets/manh.jpeg"
-              }
-            }
-        },
-        {   
-            title: "May 1950",
-            cardTitle: "Dunkirk",
-            cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-            cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-            // media: {
-            //   type: "IMAGE",
-            //   source: {
-            //     url: "http://someurl/image.jpg"
-            //   }
-            // }
-        }
-    ];
+    let workIconStyles = { background: "#06D6A0" };
+    let schoolIconStyles = { background: "#f9c74f" };
+
     return (
         <div className="experience" id="experience">
-            <h1>Experience</h1>
-            <div className="timeline">
-                <div className="left">
+            <h1>My Journey</h1>
+            <VerticalTimeline>
+        {timelineElements.map((element) => {
+          let isWorkIcon = element.icon === "work";
+          let showButton =
+            element.buttonText !== undefined &&
+            element.buttonText !== null &&
+            element.buttonText !== "";
 
-                </div>
-                <div className="divide">
-                    In progress
-                </div>
-                <div className="right">
-
-                </div>
-            </div>
-            {/* <div className="timeline">
-                <Chrono 
-                    items={items}   
-                    theme={{ 
-                        primary: "blue",
-                        secondary: "black",
-                        cardBgColor: "rgb(248, 240, 227)",
-                        cardForeColor: "black",
-                        titleColor: "white"
-                    }}
-                    mode="VERTICAL_ALTERNATING"
-                    scrollable="false"
-                    useReadMore="false"
-                    enableOutline="true"
-                    hideControls="true"
-                    borderLessCards="false"
-                    disableNavOnKey="true"
-                    allowDynamicUpdate="true"
-                />
-            </div> */}
+          return (
+            <VerticalTimelineElement
+              key={element.key}
+              date={element.date}
+              dateClassName="date"
+              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+              icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                {element.title}
+              </h3>
+              <h5 className="vertical-timeline-element-subtitle">
+                {element.location}
+              </h5>
+              <p id="description">{element.description}</p>
+              {showButton && (
+                <a
+                  className={`button ${
+                    isWorkIcon ? "workButton" : "schoolButton"
+                  }`}
+                  href="/"
+                >
+                  {element.buttonText}
+                </a>
+              )}
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
         </div>
-    )
+    );
 }
---
+++ src/timelineElements.js
@@ -0,0 +1,49 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Software Engineer - Manhattan Associates",
+    location: "Atlanta, Georgia",
+    description:
+      "Joined the Movers team heading the Putaway component, collaborating with team members overseas. Practice Object-Oriented Programming techniques using Java and Spring Boot, creating JUnit tests. Helped implement the compatibility of multiple items at once in the Putaway component using Agile. Awarded Research & Development Team of the Quarter in April 2021",
+    date: "March 2021 - Today",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Georgia Institue of Technology",
+    location: "Atlanta, Georgia",
+    description:
+      "Transferred here to finish my Computer Science degree. Graduated with a 3.85 and Highest Honors. Active in Sigma Nu, and IM sports.",
+    date: "January 2019 - December 2020",
+    icon: "school",
+  },
+  {
+    id: 3,
+    title: "DTT Enablement - Chick-fil-A Corporate",
+    location: "Atlanta, Georgia",
+    description:
+      "Joined the Identity and Access Management Team to build automation tools for managing user accounts. Used AWS Lambda and CloudFormation with Python to make API calls in a Jenkins environment. Utilized Splunk to build alerts from SQL queries for deactivating inactive user accounts",
+    date: "June 2020 - July 2020",
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "University of Georgia",
+    location: "Athens, Georgia",
+    description:
+      "College - Attended with a 3.91 GPA before transferring out. Participated in Filipino Student Association, HEROs, and IM Soccer, Flag Football, and Ultimate Frisbee.",
+    date: "August 2017 - December 2018",
+    icon: "school",
+  },
+  // {
+  //   id: 5,
+  //   title: "Oconee County High School",
+  //   location: "Watkinsville, Georgia",
+  //   description:
+  //     "Highschool - Graduate Summa Cum Laude. Recieved Georgia Scholar's Award and AP Scholar. Participated in Youth Leadership Oconee, Teen Oconee, Beta Club, Track and Field, and FBLA",
+  //   date: "2013 - 2017",
+  //   icon: "school",
+  // },
+];
+
+export default timelineElements;
